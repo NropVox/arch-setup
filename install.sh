4bd238c1b230376a66e9cfd68317afd586cb4e0f
IstarVin <ajdeleverio20@gmail.com>
2023-05-12 07:58:18 +0800

    add pacman conf

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+#setup pacman
+pacman_conf=/etc/pacman.conf
+sed '/s/#ParallelDownloads/ParallelDownloads/' $pacman_conf > $pacman_conf.tmp
+mv $pacman_conf $pacman_conf.bak
+mv $pacman_conf.tmp $pacman_conf
+
+#DEBUG
+exit
+
 configpath=archinstall-configs
 config=$configpath/user_configuration.json
 creds=$configpath/user_credentials.json
