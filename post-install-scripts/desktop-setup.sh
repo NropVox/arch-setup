22daa0c0d5914bec3f08b817dea05a0e44003a62
IstarVin <ajdeleverio20@gmail.com>
2023-05-13 02:46:01 +0800

    fix

--- post-install-scripts/desktop-setup.sh
+++ post-install-scripts/desktop-setup.sh
@@ -1,15 +1,11 @@
 set -e
-#test
-read -s -p "Password: " password
 
 # install yay
 cd /opt
 git clone https://aur.archlinux.org/yay-git.git
 chown aj:aj yay-git
 cd yay-git
-sudo -u aj makepkg -si --noconfirm << EOF
-$password
-EOF
+sudo -u aj makepkg -si --noconfirm
 
 # install gnome
 
