17526ba1c1a5e9c80ab0d24685f51d98fc57392c
IstarVin <ajdeleverio20@gmail.com>
2023-05-12 22:02:30 +0800

    modularity

--- post-install-scripts/desktop-setup.sh
+++ post-install-scripts/desktop-setup.sh
@@ -1,6 +1,11 @@
-#test
+sed -e
+
 
 # install yay
+mkdir /mnt/archinstall/home/aj/.pis -p
+cp post-install-scripts/* /mnt/archinstall/home/aj/.pis
+
+arch-chroot /mnt/archinstall "/home/aj/.pis/desktop-setup.sh"
 
 # install gnome
 
