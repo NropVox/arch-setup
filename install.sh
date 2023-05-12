2688612f6e3220d0b6b6e3377fcbd09b27c477c0
IstarVin <ajdeleverio20@gmail.com>
2023-05-12 08:13:01 +0800

    fix

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ set -e
 
 #setup pacman
 pacman_conf=/etc/pacman.conf
-sed '/s/#ParallelDownloads/ParallelDownloads/' $pacman_conf > $pacman_conf.tmp
+sed 's/#ParallelDownloads/ParallelDownloads/' $pacman_conf > $pacman_conf.tmp
 mv $pacman_conf $pacman_conf.bak
 mv $pacman_conf.tmp $pacman_conf
 
