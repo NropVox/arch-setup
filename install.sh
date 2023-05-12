711648141cad429e2f38e00179dfe25cff6ffbd4
IstarVin <ajdeleverio20@gmail.com>
2023-05-12 08:21:09 +0800

    remove debug

--- install.sh
+++ install.sh
@@ -8,9 +8,6 @@ sed 's/#ParallelDownloads/ParallelDownloads/' $pacman_conf > $pacman_conf.tmp
 mv $pacman_conf $pacman_conf.bak
 mv $pacman_conf.tmp $pacman_conf
 
-#DEBUG
-exit
-
 configpath=archinstall-configs
 config=$configpath/user_configuration.json
 creds=$configpath/user_credentials.json
