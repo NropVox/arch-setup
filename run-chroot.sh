412848a5c7fe183641ab743b0affaf1588d208d0
IstarVin <ajdeleverio20@gmail.com>
2023-05-12 19:04:21 +0800

    fix

--- run-chroot.sh
+++ run-chroot.sh
@@ -2,4 +2,4 @@ set -e
 
 cp post-install-scripts/* /mnt/archinstall/home/aj/.pis
 
-arch-chroot /mnt/archinstall /home/aj/.pis/desktop-setup.sh
+arch-chroot /mnt/archinstall "bash /home/aj/.pis/desktop-setup.sh"
