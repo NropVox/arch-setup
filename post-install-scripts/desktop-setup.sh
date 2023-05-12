715832af0d1f83bd2808fda5ff7d6edbfc83b5ca
IstarVin <ajdeleverio20@gmail.com>
2023-05-13 02:53:20 +0800

    install gnome ly

--- post-install-scripts/desktop-setup.sh
+++ post-install-scripts/desktop-setup.sh
@@ -1,14 +1,13 @@
 set -e
 
 #remove pass from user to skip sudo password
-passwd -d aj
+#passwd -d aj
 
 # install yay
-cd /opt
-git clone https://aur.archlinux.org/yay-git.git
-chown aj:aj yay-git
-cd yay-git
-sudo -u aj makepkg -si --noconfirm
 
 # install gnome
+pacman -S gnome-shell gnome-terminal nautilus gnome-system-monitor gnome-control-center gvfs gvfs-mtp gvfs-smb
 
+# install display manager
+yay -S ly --noconfirm
+systemctl enable ly
