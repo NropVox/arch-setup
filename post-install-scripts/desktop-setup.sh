7947fe6b72bf1d34a6e6b7ade227d1ce585c86dc
IstarVin <ajdeleverio20@gmail.com>
2023-05-13 02:17:09 +0800

    asd

--- post-install-scripts/desktop-setup.sh
+++ post-install-scripts/desktop-setup.sh
@@ -2,10 +2,11 @@ set -e
 
 
 # install yay
-mkdir /mnt/archinstall/home/aj/.pis -p
-cp post-install-scripts/* /mnt/archinstall/home/aj/.pis
-
-arch-chroot /mnt/archinstall "/home/aj/.pis/desktop-setup.sh"
+cd /opt
+git clone https://aur.archlinux.org/yay-git.git
+chown aj:aj yay-git
+cd yay-git
+sudo -u aj makepkg -si --noconfirm
 
 # install gnome
 
